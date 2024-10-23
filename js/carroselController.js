5919cf7fd975c1d5175545b606026c6f9af2a236
Leo <nickname.player15@gmail.com>
2024-10-23 15:44:35 -0300

    img ate hera

--- js/carroselController.js
+++ js/carroselController.js
@@ -1,7 +1,9 @@
-import { zeus, hades } from './bdNpc.js';
+import { zeus, hades, posseidon, hera } from './bdNpc.js';
 import { initializeSlider } from './fichaFunctions.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     initializeSlider('zeus', zeus, 1);
     initializeSlider('hades', hades, 1);
+    initializeSlider('posseidon', posseidon, 1);
+    initializeSlider('hera', hera, 1);
 });
