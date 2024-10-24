4460521857a4e44e01672a4c16d65594311ccbb6
Leo <nickname.player15@gmail.com>
2024-10-24 14:31:07 -0300

    npc recorrentes

--- js/carroselController.js
+++ js/carroselController.js
@@ -1,7 +1,8 @@
-import { zeus, hades, posseidon, hera } from './bdNpc.js';
+import { apoio, zeus, hades, posseidon, hera } from './bdNpc.js';
 import { initializeSlider } from './fichaFunctions.js';
 
 document.addEventListener("DOMContentLoaded", function() {
+    initializeSlider('apoio', apoio, 1);
     initializeSlider('zeus', zeus, 1);
     initializeSlider('hades', hades, 1);
     initializeSlider('posseidon', posseidon, 1);
