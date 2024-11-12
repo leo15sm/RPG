b6bd456911e0a25883f411066f2c58cf930e8fc0
Leo <nickname.player15@gmail.com>
2024-11-12 10:28:21 -0300

    dados até 11-11 dia 39

--- js/bdFicha.js
+++ js/bdFicha.js
@@ -20,66 +20,74 @@ export const players = [
         "Breno", "Solon", 
         new Bild("Delfos", "Feiticeiro", "Sacerdote", "Oráculo", "Cor Laranja"),
         new Atributos(10, 12, 20, 14, 16, 14),
-        new Progresso(0, 820, 9, 1, 21, 11.00),
+        new Progresso(4, 184, 1, 2, 39, 18.00),
        ),
     /*###########################################*/
     new Personagem(
         "Davizinho", "---", 
         new Bild("Harpocretes", "Ladino", "---", "---", "---"),
-        new Atributos(0, 0, 0, 0, 0, 0),
-        new Progresso(0, 87, 19.4, 0, 0, 0.50),
+        new Atributos(14, 16, 20, 14, 10, 12),
+        new Progresso(0, 114, 22.8, 0, 0, 0.50),
        ),
     /*###########################################*/
     new Personagem(
         "Ernani", "Necron", 
         new Bild("Thanatos", "Mago", "Arcanista", "Morte", "Combustão"),
         new Atributos(8, 10, 12, 20, 14, 14),
-        new Progresso(0, 246, 8.4, 0, 9, 3.24),
+        new Progresso(0, 308, 16.8, 0, 11, 10.48),
        ),
     /*###########################################*/
     //nivel, xp, skill, upgrade, missao, gold
     new Personagem(
         "Joãozinho", "Tayson", 
-        new Bild("Tritão", "Druida", "Militar", "Tritão Lagosta", "Vapor"),
-        new Atributos(0, 0, 0, 0, 0, 0),
-        new Progresso(0, 87, 19.4, 0, 0, 0.50),
+        new Bild("Tritão", "Druida", "Domador", "Tritão Lagosta", "Vapor"),
+        new Atributos(16, 12, 14, 14, 20, 10),
+        new Progresso(0, 127, 11.6, 0, 5, 1.50),
        ),
     /*###########################################*/
     new Personagem(
         "Lucas Black", "Naguine", 
         new Bild("Medusa", "Druida", "Domador", "Górgona", "Veneno"),
         new Atributos(10, 14, 12, 14, 20, 16),
-        new Progresso(0, 340, 25, 0, 7, 22.28),
+        new Progresso(0, 403, 33.4, 0, 10, 31.28),
        ),
     /*###########################################*/
     new Personagem(
-        "Lucas White", "Kagaho", 
-        new Bild("Hebe", "Espadachim", "Militar", "Fogo", "Raio"),
-        new Atributos(20, 16, 10, 14, 12, 14),
-        new Progresso(0, 205, 33.5, 2, 6, 10.20),
+        "Lucas White", "Enel", 
+        new Bild("Tempesta", "Feiticeiro", "Arcanista", "Ninfa do Raio", "Raio Vermelho"),
+        new Atributos(12, 14, 20, 16, 14, 10),
+        new Progresso(1, 164, 16.4, 1, 5, 1.00),
        ),
     /*###########################################*/
     new Personagem(
         "Lyann Black", "Lucius", 
         new Bild("Delfos", "Arqueiro", "Caçador", "Oráculo", "Cor Vermelha"),
         new Atributos(10, 12, 20, 14, 16, 14),
-        new Progresso(0, 235, 33.5, 0, 7, 3.25),
+        new Progresso(1, 349, 27.6, 1, 11, 6.50),
        ),
     /*###########################################*/
     new Personagem(
         "Lyann Nix", "Neko", 
-        new Bild("Nike", "Mago", "Acadêmico | Arcanista", "Vitória", "Madeira"),
+        new Bild("Nike", "Mago", "Acadêmico lv10", "Vitória", "Madeira"),
         new Atributos(8, 10, 14, 20, 16, 12),
-        new Progresso(0, 920, 8.6, 1, 26, 7.10),
+        new Progresso(4, 439, 30.2, 3, 44, 14.20),
        ),
     /*###########################################*/
     new Personagem(
         "Niket", "Kazuma", 
         new Bild("Ígnea", "Lanceiro", "Armeiro", "Ninfa do Fogo", "Plasma"),
         new Atributos(10, 14, 20, 12, 14, 16),
-        new Progresso(0, 387, 5.9, 0, 3, 3.00),
+        new Progresso(1, 497, 3.1, 0, 7, 6.00),
        ),
     /*###########################################*/
+    new Personagem(
+        "Uilian", "---", 
+        new Bild("Eros", "Espadachim", "Artista", "---", "Vento | Água"),
+        new Atributos(14, 14, 16, 12, 10, 20),
+        new Progresso(0, 114, 22.8, 0, 0, 0.50),
+       ),
+    /*###########################################*/
+    //dia 38 -> 114 XP e 22.8 Skill 
 ];
 
 //player, nome, bild, atributos, progresso
