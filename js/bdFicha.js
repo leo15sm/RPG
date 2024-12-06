8aa6b805497e32159211c16a69c9eb7c0eab407e
Leo <nickname.player15@gmail.com>
2024-12-06 12:54:08 -0300

    update dia 120

--- js/bdFicha.js
+++ js/bdFicha.js
@@ -7,6 +7,7 @@ export const atributos = [
     },
 ];
 //nivel, xp, skill, upgrade, missao, gold
+//forc, vit, des, int, sab, car
 export const players = [
     new Personagem(
      "Player", "Nome", 
@@ -17,67 +18,67 @@ export const players = [
     ),
      /*###########################################*/
      new Personagem(
-        "Breno", "Solon", 
-        new Bild("Delfos", "Feiticeiro", "Sacerdote lv6", "Oráculo", "Cor Laranja"),//+10 sacerdote
-        new Atributos(10, 12, 20, 15, 17, 14),
-        new Progresso(5, 457, 9.4, 1, 96, 83.00),
+        "Drock Breno", "Solon", 
+        new Bild("Delfos", "Feiticeiro", "Atleta Lv 5", "Oráculo", "Cor Laranja"),//Acadêmico Bastião, Sacerdote lv10
+        new Atributos(10, 16, 18, 14, 20, 12),
+        new Progresso(8, 667, 2.6, 12, 114, 1113.00),
        ),
     /*###########################################*/
     new Personagem(
         "Ernani", "Necron", 
-        new Bild("Thanatos", "Mago", "Arcanista lv5", "Morte", "Combustão"),
+        new Bild("Thanatos", "Mago", "Arcanista lv8", "Morte", "Combustão"),
         new Atributos(8, 10, 12, 20, 14, 14),
-        new Progresso(1, 613, 30, 3, 46, 75.48),
+        new Progresso(2, 318, 8, 6, 46, 1105.48),
        ),
     /*###########################################*/
     //nivel, xp, skill, upgrade, missao, gold
     new Personagem(
-        "Joãozinho", "Tayson", 
-        new Bild("Tritão", "Druida", "Arcanista lv2", "Tritão Lagosta", "Vapor"), //Domador lv 1
+        "João do Caos", "Tayson", 
+        new Bild("Tritão", "Druida", "Arcanista lv7", "Tritão Lagosta", "Vapor"), //Domador lv 1
         new Atributos(16, 12, 14, 14, 20, 10),
-        new Progresso(0, 437, 35, 2, 6, 1.50),
+        new Progresso(1, 142, 2, 6, 6, 1.50),
        ),
     /*###########################################*/
     new Personagem(
-        "Lucas Black", "Naguine", 
-        new Bild("Medusa", "Druida", "Domador lv2", "Górgona", "Veneno"),
+        "Luket", "Naguine", 
+        new Bild("Medusa", "Druida", "Domador lv5", "Górgona", "Veneno"),
         new Atributos(10, 14, 12, 14, 20, 16),
-        new Progresso(0, 771, 28.8, 7, 12, 141.28),
+        new Progresso(1, 96, 5, 11, 12, 230.00),
        ),
     /*###########################################*/
     new Personagem(
-        "Lucas White", "Enel", 
-        new Bild("Tempesta", "Feiticeiro", "Arcanista lv3", "Ninfa do Raio", "Raio Vermelho"),
-        new Atributos(12, 14, 20, 16, 14, 10),
-        new Progresso(1, 503, 6.1, 1, 23, 1.00),
+        "Lucas Pix", "---", 
+        new Bild("---", "---", "---", "---", "----"),
+        new Atributos(20, 20, 20, 20, 20, 20),
+        new Progresso(100, 0, 0, 0, 0, 1.00),
        ),
     /*###########################################*/
     new Personagem(
-        "Lyann Black", "Lucius", 
-        new Bild("Delfos", "Arqueiro", "Caçador lv2", "Oráculo", "Cor Vermelha"),
+        "Polo", "Lucius", 
+        new Bild("Delfos", "Arqueiro", "Caçador lv5", "Oráculo", "Cor Vermelha"),
         new Atributos(10, 12, 20, 14, 16, 14),
-        new Progresso(1, 863, 10.4, 3, 25, 71.50), //Maestria 5 Arco
+        new Progresso(3, 343, 3, 4, 25, 1000.00), //Maestria 10 Arco
        ),
     /*###########################################*/
     new Personagem(
-        "Lyann Nix", "Neko", 
-        new Bild("Nike", "Mago", "Acadêmico lv10", "Vitória", "Madeira"), //Arcanista Lv10
+        "Nix", "Neko", 
+        new Bild("Nike", "Mago", "Domador lv1", "Vitória", "Madeira"), //Arcanista Lv10, Acadêmico lv10
         new Atributos(8, 10, 14, 20, 17, 13),
-        new Progresso(5, 936, 17.0, 2, 102, 79.20),
+        new Progresso(9, 331, 2, 7, 120, 1080.00),
        ),
     /*###########################################*/
     new Personagem(
-        "Niket", "Kazuma", 
-        new Bild("Ígnea", "Lanceiro", "Armeiro lv3", "Ninfa do Fogo", "Plasma"),
-        new Atributos(10, 14, 20, 12, 14, 16),
-        new Progresso(3, 97, 20.1, 1, 48, 122.00),
+        "Yori Niket", "Kazuma", 
+        new Bild("Ígnea", "Lanceiro", "Atleta Lv5", "Ninfa do Fogo", "Flama"),//Arcanista Lv5, Militar Lv5
+        new Atributos(12, 20, 20, 15, 15, 21),
+        new Progresso(5, 117, 1, 9, 73, 1150.00),
        ),
     /*###########################################*/
     new Personagem(
         "Uilian", "Aldritch", 
-        new Bild("Eros", "Espadachim", "Artista", "Carisma", "Som"),
+        new Bild("Eros", "Feiticeiro", "Artista", "Carisma", "Ether Caos"),
         new Atributos(14, 14, 16, 12, 10, 20),
-        new Progresso(0, 229, 8.8, 1, 1, 0.50),
+        new Progresso(1, 709, 10, 5, 5, 1000.00),
        ),
     /*###########################################*/
     //dia 74 -> 219 XP e 43.8 Skill 
