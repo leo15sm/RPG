46927899ea3bee4fc79f695d177fe53c89ca7f33
Leo <nickname.pleyer15@gmail.com>
2025-02-20 22:11:57 -0300

    update 02-2025

--- public/js/bdFicha.js
+++ public/js/bdFicha.js
@@ -35,12 +35,12 @@ export const players = [
             "Delfos", 
             getClasseByName("Feiticeiro"), 
             ["Vigor"], 
-            ["Chama Dourada"], 
+            ["Chama Dourada", "Arcano do Físico"], 
             ["Elite 1", "Sacerdote 10", "Acadêmico 10", "Atleta 5", "Astrologo 5"],
             ["Arco 10", "Escudo 10", "Armadura 10"]
         ),
-        new Atributos(20, 22, 30, 30, 22, 20),
-        new Progresso(9, 201, 9, 0, 154, 1113.00)
+        new Atributos(18, 30, 29, 30, 30, 21),
+        new Progresso(10, 201, 9, 0, 154, 1113.00)
     ),
     /*###########################################*/
     new Personagem(
@@ -49,12 +49,12 @@ export const players = [
             "Thanatos", 
             getClasseByName("Mago"), 
             ["Morte"], 
-            ["Chama Sombria"], 
+            ["Chama Mágica"], 
             ["Arquimago 4"],
-            ["Foice 5"]
+            ["Foice 5", "Arco 1"]
         ),
         new Atributos(18, 22, 18, 22, 18, 18),
-        new Progresso(6, 780, 2, 1, 92, 1105.48)
+        new Progresso(7, 780, 2, 1, 92, 1105.48)
     ),
     /*###########################################*/
     new Personagem(
@@ -64,11 +64,11 @@ export const players = [
             getClasseByName("Druida"), 
             ["Tritão Lagosta"], 
             ["Aqua"], 
-            ["Atleta 10", "Arcanista 10", "Domador de Criaturas 1"],
+            ["Olympico 4", "Arcanista 10", "Domador de Criaturas 1"],
             ["Combate 10"]
         ), 
-        new Atributos(18, 18, 18, 22, 22, 18),
-        new Progresso(6, 644, 8, 11, 84, 10.00)
+        new Atributos(18, 22, 22, 30, 30, 18),
+        new Progresso(7, 644, 8, 11, 84, 10.00)
     ),
     /*###########################################*/
     //nivel, xp, skill, upgrade, missao, gold
@@ -84,7 +84,7 @@ export const players = [
             ["Chicote 5"]
         ),
         new Atributos(18, 18, 18, 22, 22, 18),
-        new Progresso(6, 720, 3, 22, 48, 230.00)
+        new Progresso(7, 720, 3, 22, 48, 230.00)
     ),
     /*###########################################*/
     new Personagem(
@@ -107,12 +107,12 @@ export const players = [
             "Delfos", 
             getClasseByName("Arqueiro"), 
             ["Oráculo"], 
-            ["Cor Vermelha"], 
-            ["Caçador 5", "Domador 10", "Faz Tudo 10", "Atleta 5"],
+            ["Cor Vermelha", "Cor Laranja", "Cor Amarela", "Chama Prateada"], 
+            ["Caçador 10", "Domador 10", "Faz Tudo 10", "Atleta 10", "Arcanista 1"],
             ["Arco 10"]
         ),
         new Atributos(18, 18, 22, 18, 22, 18),
-        new Progresso(6, 877, 1, 11, 86, 1000.00)
+        new Progresso(7, 877, 7, 2, 86, 1000.00)
     ),
     /*###########################################*/
     new Personagem(
@@ -121,12 +121,12 @@ export const players = [
             "Nike", 
             getClasseByName("Mago"), // Passando o objeto da classe "Mago"
             ["Vitória"], 
-            ["Madeira"], 
-            ["Domador de Criaturas 4", "Arquimago 1", "Faz tudo 10", "Atltelta 5"],
+            ["Madeira", "Chama Helênica"], 
+            ["Domador de Criaturas 4", "Arquimago 4", "Faz tudo 10", "Atltelta 5"],
             ["fraco 100"]
         ),
-        new Atributos(18, 18, 23, 23, 18, 18),
-        new Progresso(9, 865, 0, 16, 162, 1080.00)
+        new Atributos(18, 19, 23, 30, 30, 18),
+        new Progresso(11, 900, 0, 16, 165, 1080.00)
     ),
     /*###########################################*/
     //nivel, xp, skill, upgrade, missao, gold
@@ -142,7 +142,7 @@ export const players = [
             ["Lança 10", "Armadura 10", "Pistola 10"]
         ),
         new Atributos(18, 18, 30, 30, 22, 23),
-        new Progresso(6, 651, 0, 17, 113, 1150.00)
+        new Progresso(7, 651, 0, 17, 113, 1150.00)
     ),
     /*###########################################*/
     new Personagem(
@@ -159,9 +159,24 @@ export const players = [
         new Progresso(2, 283, 3, 11, 45, 1000.00)
     ),
     /*###########################################*/
+    /*###########################################*/
+    new Personagem(
+        "Dômia", "Dômia", 
+        new Bild(
+            "Aracne", 
+            getClasseByName("Mago"),
+            ["Vitória", "Ether"], 
+            ["Ether", "Fios"], 
+            ["Militar 5", "Arcanista 10"],
+            ["Espada 5"]
+        ),
+        new Atributos(18, 19, 23, 30, 30, 18),
+        new Progresso(11, 1000, 0, 0, 185, 1000.00)
+    ),
+    /*###########################################*/
   
 ];
-  //dia 150
+//dia 180
 //player, nome, bild, atributos, progresso
 //forc, vit, des, int, sab, car
 //ascendente, classe, dominio, elemento, oficio, maestria
